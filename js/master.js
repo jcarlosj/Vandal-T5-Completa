b54b8dbc69c636dc119e7abf8198b2a8d4da5179
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-02 10:16:46 -0500

    [+] DOM/JavaScript: Bootstrap Navbar & Button

--- js/master.js
+++ js/master.js
@@ -7,6 +7,12 @@ window .onload = () => {
         $body .insertBefore( $footer, $body .firstElementChild );      
         $body .insertBefore( $main, $body .firstElementChild );
         $body .insertBefore( $header, $body .firstElementChild );
+
+        $header .appendChild( createEl( 'div', [ 'top' ] ) );
+        $header .appendChild( createEl( 'div', [ 'bottom' ] ) );
+
+        $main .appendChild( createEl( 'section', [ 'section', 'container', 'games' ] ) );
+        $main .appendChild( createEl( 'section', [ 'section', 'container', 'news' ] ) );
         
     console .log( $body );
 
@@ -17,10 +23,35 @@ window .onload = () => {
         if( classes ) {
             classes .forEach( clase => {
                 //console .log( clase );
-                $el .setAttribute( 'class', clase );
+                $el .classList .add( clase );
             });
         }
     
         return $el;
     }
+
+    function createNavbar() {
+        let items = new Array( 'Portada', 'Noticias', 'Análisis', 'Juegos', 'Guías', 'PC', 'PS4', 'XBOne', 'Switch', 'Plataformas', 'Podcast', 'Foro', 'eSports', 'Hardware', 'Random' ),
+            $nav = createEl( 'nav', [ 'navbar', 'navbar-expand-lg', 'navbar-light', 'bg-light' ] ),
+            $button = createButton();
+
+            $button .setAttribute( 'type', 'button' );
+            $button .setAttribute( 'data-toggle', 'collapse' );
+            $button .setAttribute( 'data-target', '#main-menu' );
+            $button .setAttribute( 'aria-controls', 'navbarNavAltMarkup' ), 
+            $button .setAttribute( 'aria-expanded', 'false' ), 
+            $button .setAttribute( 'aria-label', 'Toggle navigation' ), 
+            
+            $nav .appendChild( $button );
+
+        console .log( $nav );
+        
+    }
+    function createButton() {
+        let $button = createEl( 'button', [ 'navbar-toggler' ] );
+
+        console .log( $button );
+        return $button;
+    }
+    console .log( createNavbar() );
 }
