c976539aa441f5818ff8879a0f0225422f00515e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-02 12:06:50 -0500

    [+] DOM/JavaScript: Automatiza el despliegue de los enlaces del menu principal

--- js/master.js
+++ js/master.js
@@ -12,7 +12,7 @@ window .onload = () => {
         $header .appendChild( createEl( 'div', [ 'header-bottom' ] ) );
         
         var hb = $header .lastChild;
-        hb .appendChild( createNavbar() );
+        hb .appendChild( createNavbar( $header ) );
 
         $main .appendChild( createEl( 'section', [ 'section', 'container', 'games' ] ) );
         $main .appendChild( createEl( 'section', [ 'section', 'container', 'news' ] ) );
@@ -20,9 +20,13 @@ window .onload = () => {
     console .log( $body );
 
     /** Funtions */
-    function createEl( el, classes = null ) {
+    function createEl( el, classes = null, id = null ) {
         let $el = document .createElement( el );
     
+        if( id ) {
+            $el .setAttribute ( 'id', id );
+        }
+
         if( classes ) {
             classes .forEach( clase => {
                 //console .log( clase );
@@ -33,10 +37,14 @@ window .onload = () => {
         return $el;
     }
 
-    function createNavbar() {
+    function createNavbar( $header ) {
         let items = new Array( 'Portada', 'Noticias', 'Análisis', 'Juegos', 'Guías', 'PC', 'PS4', 'XBOne', 'Switch', 'Plataformas', 'Podcast', 'Foro', 'eSports', 'Hardware', 'Random' ),
             $nav = createEl( 'nav', [ 'navbar', 'navbar-expand-lg', 'navbar-light', 'bg-light' ] ),
             $button = createButton(),
+            $span = createEl( 'span', [ 'navbar-toggler-icon' ] ),
+            $divEl1 = createEl( 'div', [ 'collapse', 'navbar-collapse' ], 'main-menu' ),
+            $divEl2 = createEl( 'div', [ 'navbar-nav' ] ),
+            $a,
             attributesButton = {
                 'type': 'button',
                 'data-toggle': 'collapse',
@@ -46,10 +54,21 @@ window .onload = () => {
                 'aria-label': 'Toggle navigation'
             };
 
-            $button = addAttributes( $button, attributesButton );
-            $nav .appendChild( $button );
+        $button = addAttributes( $button, attributesButton );
+        $nav .appendChild( $button );
 
-        //console .log( $nav );
+        $nav .lastChild .appendChild( $span );
+
+        items .forEach( item => {
+            $textContent = document .createTextNode( item );
+            $a = createEl( 'a', [ 'nav-item', 'nav-link', 'active' ] );
+            $a .appendChild( $textContent );
+            $divEl2 .appendChild( $a ); 
+        });
+
+        $divEl1 .appendChild( $divEl2 );
+        //console .log( 'Heyyyy!', $divEl2 );
+        $nav .appendChild( $divEl1 );
 
         return $nav;
     }
