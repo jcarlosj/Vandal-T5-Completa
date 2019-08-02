176384f2b2512d4d1935203e0dad95a8111154a4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-02 09:43:20 -0500

    [+] DOM/JavaScript: header, main, footer elements

--- js/master.js
+++ js/master.js
@@ -1,3 +1,26 @@
-var msg = 'Vandal Web';
-alert( msg );
-console .log( msg );
+window .onload = () => {
+    var $body = document .querySelector( 'body' ),
+        $header = createEl( 'header', [ 'header' ] ),
+        $main = createEl( 'main', [ 'main' ] ),
+        $footer = createEl( 'footer', [ 'footer' ] );
+
+        $body .insertBefore( $footer, $body .firstElementChild );      
+        $body .insertBefore( $main, $body .firstElementChild );
+        $body .insertBefore( $header, $body .firstElementChild );
+        
+    console .log( $body );
+
+    /** Funtions */
+    function createEl( el, classes = null ) {
+        let $el = document .createElement( el );
+    
+        if( classes ) {
+            classes .forEach( clase => {
+                //console .log( clase );
+                $el .setAttribute( 'class', clase );
+            });
+        }
+    
+        return $el;
+    }
+}
