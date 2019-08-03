8cd8f2de66f26252501f4178f29bfc1006877c35
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-02 19:19:35 -0500

    [$] DOM/JavaScript: Refactoriza nuevamente creacion elementos: header, div, footer

--- js/master.js
+++ js/master.js
@@ -60,29 +60,27 @@ window .onload = () => {
         }
         /** Agrega elementos: header, main y footer al body */
         createChildrenElementsBy( $parentEl, structure ) {
-            let listEl = new Array();
+            let listEl = new Array(); console .log( structure );
 
             // Itera por ID (1er Nivel de Profundidad del Objeto)
             for ( let element in structure ) {
 
                 // Valida que el element exista dentro del Objeto iterado
                 if ( structure .hasOwnProperty( element ) ) {
-                    let properties = Object .getOwnPropertyNames( structure[ element ] );    // Obtiene todas las propiedades existentes del objeto
+                    console .group( `element = ${ structure[ element ] .tag }` );
+                    console .log( `id = ${ structure[ element ] .id }` );
+                    console .log( `class = ${ structure[ element ] .class }` );
 
-                    console .group( `element = ${ element }` );
+                    let properties = Object .getOwnPropertyNames( structure[ element ] ),           // Obtiene todas las propiedades existentes del objeto
+                        $el = Element .create(                                                      // Crea elemento con sus id y clases
+                            structure[ element ] .tag, 
+                            structure[ element ] .class, 
+                            structure[ element ] .id 
+                        );
 
-                    // Itera propiedades (2do Nivel de Profundidad del Objeto)
-                    properties .forEach( property => {
-                        let id = property,
-                            classes = structure[ element ][ property ],
-                            $el = Element .create( element, classes, id );
-                            $el .appendChild( document .createTextNode( element ) );
-                           
-                        $parentEl .insertBefore( $el, $parentEl .firstElementChild );   // Inserta elementos: header, div, footer al body
-
-                        console .log( `id: ${ property }` );
-                        console .log( `class: ${ structure[ element ][ property ] }` );
-                    });
+                    $el .appendChild( document .createTextNode( structure[ element ] .tag ) );      // TEMPORAL: Solo para poner el nombre del elemento dentro del elemento
+                    $parentEl .insertBefore( $el, $parentEl .firstElementChild );                   // Inserta elementos: header, div, footer al body
+                    console .log( $parentEl );
             
                     console .groupEnd();
                 }
@@ -92,16 +90,19 @@ window .onload = () => {
         }
     }
 
-    let bodyStructure = { 
-        'header': { 'header': [ 'header', 'order-1' ] },
-        'div': { 'main-content': [ 'main-content', 'order-2' ] },
-        'footer': { 'footer': [ 'footer', 'order-3' ] } 
-    } 
+    let html = { 
+        'body': [
+            { 'tag': 'header', 'id': null, 'class': [ 'header', 'order-1' ] },
+            { 'tag': 'div', 'id': null, 'class': [ 'main-content', 'order-2' ] },
+            { 'tag': 'footer', 'id': null, 'class': [ 'footer', 'order-3' ] }
+        ]
+    };
 
+    console .log( html .header );
     let $body = Element .get( 'body' );
     let dom = new DOM( $body );
     
-    $body = dom .createChildrenElementsBy( $body, bodyStructure );
+    $body = dom .createChildrenElementsBy( $body, html .body );
 
     console .log( $body );
 
